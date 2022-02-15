d449c8e4e96219bf49a37538770ee995d9ce93c2
draganet <3417237276@qq.com>
2022-02-15 12:03:16 +0800

    deploy

---
+++ precache-manifest.c4f07aad6caa87eacee160a169933e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fbef5638fa39936b97",
+    "url": "/morney-website/css/app.b860f7c9.css"
+  },
+  {
+    "revision": "9fa67ab33382b26607054f025fe1128c",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "f8fbef5638fa39936b97",
+    "url": "/morney-website/js/app.72332e00.js"
+  },
+  {
+    "revision": "f7be4ed69fdfd556049b",
+    "url": "/morney-website/js/chunk-vendors.c00f68fe.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
